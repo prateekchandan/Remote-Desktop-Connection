335ccdba015067e08abfcf8bfda4541383510db3
prateekchandan <prateekchandan5545@gmail.com>
2014-11-01 20:47:56 +0530

    sending image in pixeldata

--- client/index.php
+++ client/index.php
@@ -2,6 +2,11 @@
 <html>
 <head>
 
+<style type="text/css">
+  #message{
+    height: 200px;
+  }
+</style>
 </head>
 <body>
 <div id="sse">
@@ -10,6 +15,8 @@
    <input id="text">
    <button onclick="send()">SEND</button>
    
+
+   <canvas id="myCanvas" width="300" height="300"></canvas>
    <script type="text/javascript">
 
 
@@ -19,7 +26,7 @@ var ws=0
   {
       document.getElementById('message').innerHTML="WEB SOCKET SUPPORTED";
      // Let us open a web socket
-     ws = new WebSocket("ws://localhost:8882");
+     ws = new WebSocket("ws://<?php echo $_SERVER['SERVER_ADDR']?>:8881");
      ws.onopen = function()
      {
         // Web Socket is connected, send data using send()
@@ -28,7 +35,9 @@ var ws=0
      ws.onmessage = function (evt) 
      { 
         var received_msg = evt.data;
-        document.getElementById('message').innerHTML+="<br>"+received_msg;
+        console.log(received_msg);
+        received_msg=JSON.parse(received_msg);
+        fillRect(received_msg)
      };
      ws.onclose = function()
      { 
@@ -63,6 +72,20 @@ var ws=0
 
 //while(1)
 //  ws.send("hello");
+</script>
+
+
+<script>
+function fillRect(myarray){
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  for (var i = 0; i < myarray.length; i++) {
+    data=myarray[i];
+    ctx.fillStyle = "rgb("+(data[2])+","+(data[3])+","+(data[4])+")";    
+    ctx.fillRect( data[0]-1, data[1]-1, data[0], data[1] );
+  };
+}
+
 </script>
 </div>
 </body>
--- test.py
+++ test.py
@@ -2,19 +2,31 @@
 
 import asyncio
 import websockets
+#import thread
 
-@asyncio.coroutine
+#@asyncio.coroutine
 def hello(websocket, path):
 	name="k"
 	while name!="exit":
 	    name = yield from websocket.recv()
-	    print("< {}".format(name))
-	    greeting = "Hello {}!".format(name)
-	    yield from websocket.send(name)
-	    yield from websocket.send(greeting)
-	    print("> {}".format(greeting))
+	    r,g,b=name.split(",") 
+	    greeting="["
+	    x=1
+	    while x<=300:
+	    	y=1
+	    	while y<=300:
+	    		if y!=1 or x!=1:
+	    			greeting+=","
+	    		greeting+="["+str(x)+","+str(y)+","+str(x)+","+str(0)+","+str(0)+"]"
+	    		y+=1
+	    	x+=1
+	    	
 
-start_server = websockets.serve(hello, 'localhost', 8882)
+	    greeting+="]"
+	    yield from websocket.send(greeting)
+	    print("sent an iamge")
+
+start_server = websockets.serve(hello, 'localhost', 8881)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
