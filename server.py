ba993af395134db3cc7a1abbaab4cd157e3917ce
prateekchandan <prateekchandan5545@gmail.com>
2014-11-16 00:50:36 +0530

    mouse initiated

--- server.py
+++ server.py
@@ -7,19 +7,20 @@ import threading
 import time
 
 
+def control_evets(msg):
+	keys,mouse,pointer = msg.split('|')
 
 @asyncio.coroutine
 def hello(websocket, path):
 	name="k"
 	while name!="exit":
 		name = yield from websocket.recv()
-		
+		control_evets(msg)
 		os.system("python testsc.py > out")
 		f=open("out",'r')
 		greeting=f.read()
 		yield from websocket.send(greeting)
 		print("image sent")
-#		print( greeting)
 
 start_server = websockets.serve(hello, '', 7861)
 i=0
