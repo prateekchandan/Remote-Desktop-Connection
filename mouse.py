6af3c03d37607c36f7a74dd1436ec3e48ecbc81c
prateekchandan <prateekchandan5545@gmail.com>
2014-11-16 02:06:04 +0530

    Mouse relative

--- mouse.py
+++ mouse.py
@@ -1,5 +1,6 @@
 from pymouse import PyMouse
 import sys
+import gtk
 
 m = PyMouse()
 
@@ -14,9 +15,17 @@ arguments= sys.argv
 arguments = [int(x) for x in arguments[1:]]
 
 
+width = gtk.gdk.screen_width()
+height = gtk.gdk.screen_height()
+
+
 if len(arguments) == 0:
 	pass
 elif  arguments[0] == 0:
+	arguments[1] = (arguments[1] * width)/1000
+	arguments[2] = (arguments[2] * height)/1000
 	MouseGo(arguments[1],arguments[2])
 elif arguments[0] == 1:
+	arguments[1] = (arguments[1] * width)/1000
+	arguments[2] = (arguments[2] * height)/1000
 	MouseClick(arguments[1],arguments[2],arguments[3])
