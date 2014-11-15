d0363811211c217d8f1206a275c3dc82ef853b10
Anurag Shirolkar <ashirolkar47@gmail.com>
2014-11-15 22:33:04 +0530

    keypress in jquery

--- client/index.php
+++ client/index.php
@@ -21,81 +21,15 @@
 <img id="ItemPreview" src="" />
    
 
-   <canvas id="myCanvas" width="1300" height="768"></canvas>
-   <script type="text/javascript">
+   <canvas id="myCanvas" width="100%" height="100%"></canvas>
 
-
-var ws=0
-
-  if ("WebSocket" in window)
-  {
-      document.getElementById('message').innerHTML="WEB SOCKET SUPPORTED";
-     // Let us open a web socket
-     ws = new WebSocket("ws://<?php echo $_SERVER['SERVER_ADDR']?>:7861");
-     ws.onopen = function()
-     {
-        // Web Socket is connected, send data using send()
-        document.getElementById('message').innerHTML+="<br>Connected";
-     };
-     ws.onmessage = function (evt) 
-     { 
-        var received_msg = evt.data;
-        var d = new Date();
-        console.log("Recieve: "+d.getTime());
-        //received_msg=JSON.parse(received_msg);
-        fillRect(received_msg)
-     };
-     ws.onclose = function()
-     { 
-        // websocket is closed.
-        document.getElementById('message').innerHTML+="<br>Connection closed By Server";
-     }
-  }
-  else
-  {
-     // The browser doesn't support WebSocket
-     document.getElementById('message').innerHTML="WEB SOCKET CONNECTION FAILED";
-  }
-  
-
-//while(1)
-//  ws.send("hello");
-</script>
 
 
 <script>
-
-
-function fillRect(myarray){
-	//alert(myarray);
-  document.getElementById("ItemPreview").src = "data:image/png;base64," + myarray;
-/*
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  for (var i = 0; i < myarray.length; i++) {
-    data=myarray[i];
-    ctx.fillStyle = "#"+data[2];    
-    ctx.fillRect( data[0]-1, data[1]-1, data[0], data[1] );
-  };*/
-}
-
-var r=0,g=0,b=0;
-function changeimage(){
-  //alert("hello");
-  var str=r+","+g+","+b
-  var d = new Date();
-  console.log("Sent: "+d.getTime());
-  ws.send(str);
-  r=(r+1)%255;
-  if(r==0)
-    g+=100
-}
-setInterval(changeimage, 100);
-
-function disconnect(){
-  ws.send("exit");
-}
+var serverAddr = "ws://<?php echo $_SERVER['SERVER_ADDR']?>:7861"
 </script>
+<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script src="js/index.js"></script>
 </div>
 </body>
 </html>
